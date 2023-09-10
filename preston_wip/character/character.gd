fc242a0975b07d50c19387a737d8027b20b22965
Tyler <t.john.harpe@gmail.com>
2023-09-10 19:26:41 -0400

    THE CIRCLE IS HERE

--- preston_wip/character/character.gd
+++ preston_wip/character/character.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 # @onready var pointer = $Pointer
 @onready var ray = $Pointer/RayCast2D 
 var hitspot
+signal hit_something 
 
 func _ready():
 	print(ray)
@@ -15,24 +16,15 @@ func get_input(delta):
 	velocity = input_direction * speed * delta * 100
 	look_at(get_global_mouse_position())
 	
-func _process(delta):
-	pass
-	
 func _physics_process(delta):
 	get_input(delta)
 	move_and_slide()
 
 	if Input.is_action_just_pressed("fire"):
-		print("shoot")
 		if ray.is_colliding():
-			print("hit") 
 			hitspot = ray.get_collision_point()
-			print(hitspot)
-			queue_redraw()
-			
+			hit_something.emit()
 
-func _draw():
-	if hitspot:
-		draw_circle(to_local(hitspot), 10, Color.RED)
-			
+
+
 
