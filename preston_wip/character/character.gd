cf63768a148524b7d86be794f6cda4ac14a65862
Preston Hale <preston@spryfox.com>
2023-09-10 14:35:37 -0700

    Add vision cone lib

--- preston_wip/character/character.gd
+++ preston_wip/character/character.gd
@@ -2,12 +2,17 @@ extends CharacterBody2D
 
 @export var speed = 400
 
+@export var debug_draw = false
+
 func get_input(delta):
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed * delta * 100
 	look_at(get_global_mouse_position())
 	
+func _process(delta):
+	if Input.is_action_pressed("fire"):
+		print("shoot")
+	
 func _physics_process(delta):
 	get_input(delta)
 	move_and_slide()
-
--- tyler_wip/RayCast2D_bullet_time.gd
+++ tyler_wip/RayCast2D_bullet_time.gd
@@ -9,7 +9,7 @@ func _ready():
 
 func _unhandled_input(event):
 	if event.is_action_pressed("fire") and ray.is_colliding(): 
-			print("shoot")
+		print("shoot")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
