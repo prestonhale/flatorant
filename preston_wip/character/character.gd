4f0329d8933a60549289250edd6c286001d08241
Tyler <t.john.harpe@gmail.com>
2023-09-09 20:19:42 -0400

    twisties

--- preston_wip/character/character.gd
+++ preston_wip/character/character.gd
@@ -2,10 +2,12 @@ extends CharacterBody2D
 
 @export var speed = 400
 
-func get_input():
+func get_input(delta):
 	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * speed
-
+	velocity = input_direction * speed * delta * 100
+	look_at(get_global_mouse_position())
+	
 func _physics_process(delta):
-	get_input()
+	get_input(delta)
 	move_and_slide()
+
