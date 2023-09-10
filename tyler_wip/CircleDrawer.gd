dbc159c1347b3d5918f4cb6894d05e65cb723320
Preston Hale <preston@spryfox.com>
2023-09-10 16:42:15 -0700

    Organization pass

--- tyler_wip/CircleDrawer.gd
+++ tyler_wip/CircleDrawer.gd
@@ -1,11 +1,18 @@
 extends Node2D
 var hitspot 
+var character_location
 
 func _draw():
 	if hitspot:
 		draw_circle(to_local(hitspot), 10, Color.RED)
+		draw_line(to_local(character_location), to_local(hitspot), Color.RED, 2)
+#		get_tree.timer
 
-func draw_hitspot(in_hitspot):
-	hitspot=in_hitspot
+func draw_hit(in_hitspot, in_character_location):
+	hitspot = in_hitspot
+	character_location = in_character_location
 	queue_redraw()
-	
+#
+#
+#func _on_cleanup_timer():
+#
